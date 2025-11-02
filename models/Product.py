9bcf701f3bd1349a31f221ff07162670ba0a75d9
OracunPB <paubs1@hotmail.com>
2025-11-02 12:28:10 +0100

    updates funcional 100% en Windows porque WSL ha petado :'(

--- models/Product.py
+++ models/Product.py
@@ -1,7 +1,7 @@
 from sqlmodel import SQLModel, Field
 
 class Product(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
+    id: int | None = Field(default=None, primary_key=True)
     name: str
     price: int
     stock: int
